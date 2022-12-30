df204201c098b87b3add275f9df16ed629dd2958
Fergus In London <fergus@fergus.london>
2022-12-30 23:51:24 +0000

    amend linting

--- tests/test_encoders.py
+++ tests/test_encoders.py
@@ -5,13 +5,12 @@ from dji_parse import encoders
 
 
 class TestEncoders:
-
     def test_encoder_outputs(self):
         for encoder in encoders.EncoderType:
             with open(f"tests/fixtures/output.{encoder.value}") as ff:
                 expected = ff.read()
 
-            with tempfile.TemporaryFile(mode='r+') as tf:
+            with tempfile.TemporaryFile(mode="r+") as tf:
                 encoders.get_encoder(encoder)(tf, fixtures.EXPECTED_POSITIONS).write()
 
                 tf.seek(0)
--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -7,9 +7,10 @@ from dji_parse import parsing
 
 
 class TestingParsing:
-
     @staticmethod
-    def _verify_dataclass(expected: dataclasses.dataclass, actual: dataclasses.dataclass):
+    def _verify_dataclass(
+        expected: dataclasses.dataclass, actual: dataclasses.dataclass
+    ):
         expected_dict = dataclasses.asdict(expected)
         actual_dict = dataclasses.asdict(actual)
 
@@ -17,21 +18,26 @@ class TestingParsing:
             assert expected_dict[k] == actual_dict[k]
 
     def test_metadata_from_ffmpeg_probe(self):
-        metadata = parsing.parse_metadata_from_probe({
-            "format": {
-                "duration": "23.3",
-                "tags": {
-                    "creation_time": fixtures.TEST_DATETIME.isoformat(),
-                    "location": "England, UK"
+        metadata = parsing.parse_metadata_from_probe(
+            {
+                "format": {
+                    "duration": "23.3",
+                    "tags": {
+                        "creation_time": fixtures.TEST_DATETIME.isoformat(),
+                        "location": "England, UK",
+                    },
                 }
             }
-        })
+        )
 
-        self._verify_dataclass(parsing.Metadata(
-            creation_time=fixtures.TEST_DATETIME,
-            duration=23.3,
-            location="England, UK"
-        ), metadata)
+        self._verify_dataclass(
+            parsing.Metadata(
+                creation_time=fixtures.TEST_DATETIME,
+                duration=23.3,
+                location="England, UK",
+            ),
+            metadata,
+        )
 
     def test_position_from_subtitle(self):
         subs = pysrt.open(fixtures.SUBTITLES_FILE)
@@ -40,5 +46,5 @@ class TestingParsing:
         for i in range(len(subs)):
             self._verify_dataclass(
                 fixtures.EXPECTED_POSITIONS[i],
-                parsing.position_from_subtitle(fixtures.TEST_DATETIME, subs[i])
+                parsing.position_from_subtitle(fixtures.TEST_DATETIME, subs[i]),
             )
